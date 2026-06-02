6caafa30b1089f9fcd934a4e7913188972d58691
Danish Hafeez <devmastersinstitue@gmail.com>
2026-06-02 10:40:56 +0500

    setup new backend or frontend

---
+++ restaurant-erp-backend/src/main/java/com/devmasters/restaurant_erp/RestaurantErpApplication.java
@@ -0,0 +1,13 @@
+package com.devmasters.restaurant_erp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantErpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantErpApplication.class, args);
+	}
+
+}
---
+++ restaurant-erp-backend/src/test/java/com/devmasters/restaurant_erp/RestaurantErpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devmasters.restaurant_erp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantErpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
