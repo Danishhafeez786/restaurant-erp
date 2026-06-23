32b5b99d4aa926a43a1d7c7cda68b551b339a177
Danish Hafeez <devmastersinstitue@gmail.com>
2026-06-23 11:36:41 +0500

    fix network issue to easily login any device to login application

--- restaurant-erp-backend/src/main/java/com/devmasters/restaurant_erp/config/SecurityConfig.java
+++ restaurant-erp-backend/src/main/java/com/devmasters/restaurant_erp/config/SecurityConfig.java
@@ -35,7 +35,8 @@ public class SecurityConfig {
         configuration.setAllowedOrigins(Arrays.asList(
                 "http://localhost:5173",
                 "http://192.168.100.3:5173",
-                "http://192.168.24.68:5173"
+                "http://192.168.24.68:5173",
+                "http://192.168.100.112:5173"
         ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
