c979861a34a5bc1332dc192a0146715e837c7f56
Tehseen <tehseenm88@gmail.com>
2026-07-18 11:19:14 +0500

    Update Security Config For Authentication

--- restaurant-erp-backend/src/main/java/com/devmasters/restaurant_erp/config/SecurityConfig.java
+++ restaurant-erp-backend/src/main/java/com/devmasters/restaurant_erp/config/SecurityConfig.java
@@ -23,6 +23,13 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
+//        http
+//                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+//                .csrf(csrf -> csrf.disable())
+//
+//                .authorizeHttpRequests(auth -> auth
+//                        .anyRequest().permitAll());
+
         http
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(csrf -> csrf.disable())
